0c948384968b300d093ff8afbb5ae7d8b1b421f1
ysshimx2 <ysshimx2@naver.com>
2025-03-27 17:48:57 +0900

    SeatPage 출발역 도착역 클래스

--- lib/SeatPage.dart
+++ lib/SeatPage.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class SeatPage extends StatefulWidget {
+  final String departureStation;
+  final String arrivalStation;
+
+  const SeatPage({
+    super.key,
+    required this.departureStation,
+    required this.arrivalStation,
+  });
+
+  @override
+  State<SeatPage> createState() => _SeatPageState();
+}
+
+//출발역 도착역 클래스스
